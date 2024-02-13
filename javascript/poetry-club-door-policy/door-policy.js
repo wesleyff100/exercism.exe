bfb61a3f829ec48aa973dbce711b1720eaa48b86
Wesley <wesleyff100@gmail.com>
2024-02-12 21:05:47 -0300

    feat:poetry-club

---
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -0,0 +1,46 @@
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the front door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function frontDoorResponse(line) {
+  return line[0]
+}
+
+/**
+ * Format the password for the front-door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the front door password
+ */
+let word = 'SUMMER'
+
+export function frontDoorPassword(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+}
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the back door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function backDoorResponse(line) {
+  throw new Error('Remove this line and implement the function');
+}
+
+/**
+ * Format the password for the back door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the back door password
+ */
+export function backDoorPassword(word) {
+  throw new Error('Remove this line and implement the function');
+}
